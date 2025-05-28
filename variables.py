3445735e80e7e73c6790af7a3e625931b75f7c60
moredomin <48621670@sanjoseadoratrices.edu.ar>
2025-05-28 18:02:10 -0300

    Clase 1- Variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+#Numeros enteros
+num1= 10
+
+#Numeros flotantes
+flo1= 10.5
+
+#String
+cadena= "Hola Python"
+
+#Booleanos
+booleano= True 
+
+numero = input ("Ingrese un numero: ")
+print(numero + cadena)
